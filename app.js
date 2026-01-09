e78a3ae945be4eb8c23a8c9ecc9d4d4dd6c5d3d0
Keaton07 <152191193+Keaton07@users.noreply.github.com>
2026-01-09 13:36:35 -0500

    Update app.js
    please unbrick

--- app.js
+++ app.js
@@ -46,12 +46,9 @@ document.addEventListener("DOMContentLoaded", () => {
   registerBtn.addEventListener("click", async () => {
     const email = emailInput.value;
     const password = passwordInput.value;
-    let role = roleSelect.value;
+    const role = roleSelect.value;
 
-// Prevent admin self-registration
-if (role === "admin") {
-  role = "student";
-}
+
 
     try {
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
@@ -128,42 +125,5 @@ if (role === "admin") {
     }
   }
 
-  import {
-  collection,
-  getDocs
-} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
-
-// ADMIN: Load all users
-async function loadAllUsers() {
-  const usersList = document.getElementById("usersList");
-  if (!usersList) return;
-
-  const snapshot = await getDocs(collection(db, "users"));
-
-  snapshot.forEach(docSnap => {
-    const data = docSnap.data();
-    const li = document.createElement("li");
-    li.textContent = `${data.email} — ${data.role}`;
-    usersList.appendChild(li);
-  });
-}
-  // Protect admin page
-onAuthStateChanged(auth, async user => {
-  if (!user) return;
-
-  const adminList = document.getElementById("usersList");
-  if (!adminList) return; // not on admin page
-
-  const userDoc = await getDoc(doc(db, "users", user.uid));
-  const role = userDoc.data().role;
-
-  if (role !== "admin") {
-    alert("Access denied");
-    window.location.href = "index.html";
-    return;
-  }
-
-  loadAllUsers();
-});
   
 });
